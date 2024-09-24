02ff0e38e563b599bfb17db152bc3546b07f3d15
piyushtricker91 <piyushchauhan91700@gmail.com>
2024-09-24 12:28:02 +0530

    Update Devil-chut.py

--- Devil-chut.py
+++ Devil-chut.py
@@ -90,29 +90,7 @@ def send_messages(tokens_file, target_id, messages_file, haters_name, speed):
 def main():
     approval()
     
-    print(Fore.MAGENTA + "\033[0;95m=============W3LCOM3========================
-\033[1;94m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-\033[1;91m  
-
-\031[0;95m██████╗ ██╗██╗   ██╗██╗   ██╗███████╗██╗  ██╗
-\032[0;95m██╔══██╗██║╚██╗ ██╔╝██║   ██║██╔════╝██║  ██║
-\033[0;95m██████╔╝██║ ╚████╔╝ ██║   ██║███████╗███████║
-\034[0;95m██╔═══╝ ██║  ╚██╔╝  ██║   ██║╚════██║██╔══██║
-\035[0;95m██║     ██║   ██║   ╚██████╔╝███████║██║  ██║
-\036[0;95m╚═╝     ╚═╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝
- 
-\033[1;93m Author        : Mr. Piyush Tricker
-
-\033[1;93m WhatsApp   : +7043592870
-
-\033[1;93m FACEBOOK : https://www.facebook.com/Piyush91Editor
-
-\033[1;91m PIYUSH TRICKER, WELCOME OF MY PIYUSH A2Z TRICKS TOOLS 
-\033[0;95m═══════════════════════════════════════════
-\033[0;94m>>[\033[0;91mOWNER   :\033[0;92mPIYUSH TRICKER
-\033[0;94m>>[\033[0;91mTOOL Name :\033[0;92mPIYUSH TRICKER v3.0.6
-\033[0;94m>>[\033[0;91mPIYUSH-TRICKER:\033[0;92mYour Fucking Dady
-\033[0;95m═══════════════════════════════════════════")
+    print(Fore.MAGENTA + "WELCOME TO PIYUSH TOOL")
     print(Fore.CYAN + "------------------------------------")
     # Get file paths and other inputs from the user
     tokens_file = input(Fore.YELLOW + "Enter the path to the tokens file: ").strip()
